1890e2ffb06cacd58dac2d1023289e68b2d8649e
Ebenmars <76241509+Ebenmars@users.noreply.github.com>
2023-10-23 11:32:32 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,42 @@ use std::io;
 
 fn main() {
 
-    println!("Enter  Numbers");
-    read
+    println!("Enter number 1");
+    let num1 = read_int();
+    println!();
+    println!("Enter number 2");
+    let num2 = read_int();
+    println!();
     println!("What would you like to do?");
     println!("1.Add");
     println!("2.Subtract");
     println!("3.Divide");
     println!("4.Multiply");
+    let choice = read_int();
+    println!();
+
+    if choice == 1 {
+        println!("The answer for {} + {} is {} ",num1,num2,num1 + num2);
+    }
+    else if choice == 2{
+        println!("The answer for {} - {} is {} ",num1,num2,num1 - num2);
+    }
+    else if choice == 3{
+        println!("The answer for {} / {} is {} ",num1,num2,num1 / num2);
+    }
+    else if choice == 4{
+        println!("The answer for {} * {} is {} ",num1,num2,num1 * num2);
+    }
+    else{
+        println!("Cant do anything");
+    }
 
 }
 
-fn read_int(){
+fn read_int() -> u32{
     let mut input  = String::new();
     io::stdin()
     .read_line(&mut input).expect("");
-input.trim();
+let number = input.trim().parse().expect("number");
+number
 }
